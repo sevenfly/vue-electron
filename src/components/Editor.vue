979a892a85a470cf7deea6079bd199fbbd5bc860
qibeibei <qibeibei@360.cn>
2020-01-16 17:00:12 +0800

    resize editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,34 +3,45 @@
 </template>
 
 <script>
-import { Vue, Component, Watch } from 'vue-property-decorator'
-import * as monaco from 'monaco-editor'
-export default {
-  name: 'Editor',
-  mounted () {
-    var monacoEditor = monaco.editor.create(document.getElementById('editor'), {
-      value: [
-        'function x() {',
-        '\tconsole.log("Hello world!");',
-        '}'
-      ].join('\n'),
-      language: 'javascript',
-      wrappingColumn: 0,
-      wrappingIndent: 'indent'
-    })
-    // 自适应宽度
-    window.onresize = function () {
-			    if (monacoEditor) {
-			        monacoEditor.layout()
-			    }
-    }
-  }
-}
-
+	import {
+		Vue,
+		Component,
+		Watch
+	} from 'vue-property-decorator'
+	import * as monaco from 'monaco-editor'
+	export default {
+		name: 'Editor',
+		mounted() {
+			var monacoEditor = monaco.editor.create(document.getElementById('editor'), {
+				value: [
+					'function x() {',
+					'\tconsole.log("Hello world!");',
+					'}'
+				].join('\n'),
+				language: 'javascript',
+				wrappingColumn: 0,
+				wrappingIndent: 'indent',
+				fontSize:14,
+				autoIndent:true,
+				folding:true,
+				foldingStrategy:'indentation',
+				minimap:{
+					enable:false
+				}
+				
+			})
+			// 自适应宽度
+			window.onresize = function() {
+				if (monacoEditor) {
+					monacoEditor.layout()
+				}
+			}
+		}
+	}
 </script>
 
 <style scoped>
-	#editor{
+	#editor {
 		height: 100%;
 		text-align: left;
 	}
